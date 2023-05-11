438b6593ca59a3f166bcc7132d08600e1568226d
Johnny <johnnylabedzki@hotmail.com>
2023-05-10 20:36:29 -0700

    feat: implement functionality of google search bar

--- src/components/GoogleSearchBar/GoogleSearchBar.tsx
+++ src/components/GoogleSearchBar/GoogleSearchBar.tsx
@@ -6,14 +6,44 @@ import {
   useTheme,
 } from '@mui/material';
 import { SearchOutlined } from '@mui/icons-material';
+import { useState } from 'react';
 
 export default function GoogleSearchBar() {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    if (!query) return;
+    const isUrl =
+      /^(https?:\/\/)?(www\.)?([a-z0-9-]+(\.[a-z0-9-]+)*\.[a-z]+)(\/[^\s]*)?$/.test(
+        query
+      );
+
+    if (isUrl) {
+      if (!query.startsWith('www') || !query.startsWith('https')) {
+        window.location.href = `https://${query}`;
+      } else {
+        window.location.href = query;
+      }
+      return;
+    }
+
+    const searchParams = new URLSearchParams({ q: query }).toString();
+    window.location.replace(`https://www.google.com/search?${searchParams}`);
+  };
+
   const theme = useTheme();
   return (
     <FormControl sx={{ width: 'inherit', maxWidth: '550px' }}>
       <TextField
         fullWidth
         placeholder="Search Google or type a URL"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleSearch();
+          }
+        }}
         color="primary"
         autoComplete="off"
         sx={{
